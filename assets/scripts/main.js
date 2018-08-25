2d928b495fe1b5c9eac1a0e835972176902e15b7
M-srivatsa <srivatsa.dev@gmail.com>
2018-08-25 10:28:13 +0530

    added a floating back-to-top button & changed the padding of the home section to make it fit the screen & large devices

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,3 +24,19 @@ $(document).ready(function(){
       }
     });
   });
+
+
+// This section contains the script for the floating Back-To-Top button
+  $(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
